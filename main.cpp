67fb72c7d714bc40759d5cce9c528cb6a6ea8946
Dylan Leifer-Ives <40322629+dleiferives@users.noreply.github.com>
2021-03-17 16:41:49 -0700

    My my birdie added more things.. aka thing

--- main.cpp
+++ main.cpp
@@ -10,12 +10,92 @@ struct chess_piece
   int x; // the position on the board the x
   int y; // the position on the board the y
   
- char *retrieve_name(){};
+const char * retrieve_name(char input) // retrieves the name and also like fills up the id. dont worry about it I guess.
+ {
+   switch (input)
+   {
+     case '.' :
+      return "Empty";
+      break;
+     case 'Y' :
+      return "Antelope";
+      break;
+     case 'i' :
+      return "Pawn";
+      break;
+     case '0' :
+      return "Elephant";
+      break;
+     case 'M' :
+      return "Machine";
+      break;
+     case 'I' :
+      return "Rook";
+      break;
+     case 'K' :
+      return "Knight";
+      break;
+     case '!' :
+      return "Bishop";
+      break;
+     case 'u' :
+      return "Ship";
+      break;
+     case '=' :
+      return "Buffoon";
+      break;
+     case '+' :
+      return "King";
+      break;
+     case '?' :
+      return "Queen";
+      break;
+     case 'j' :
+      return "Centurion";
+      break;
+     case '%' :
+      return "Lion";
+      break;
+     case '@' :
+      return "Gryphon";
+      break;
+     case 'C' :
+      return "Camel";
+      break;
+     case ')' :
+      return "Bow";
+      break;
+     case '<' :
+      return "Bull";
+      break;
+     case '/' :
+      return "Cannon";
+      break;
+     case ':' :
+      return "Buffalo";
+      break;
+     case '1' :
+      return "Rhinoceros";
+      break;
+     case '*' :
+      return "Star";
+      break;
+     case '#' :
+      return "Unicorn";
+      break;
+     case '$' :
+      return "Dragon Woman";
+      break;
+     case '&' :
+      return "Diablo";
+      break;
+   }
+ };
 
 };
 
 /*Global Variables*/
-char board[16][16]; // our board
+chess_piece board[16][16]; // our board
 
 /* Class Definitions */
 
@@ -23,13 +103,13 @@ class Chess_Board
 {
   public:
     /* Function Definitions */
-    bool update_board(char board[16][16]);
+    bool update_board(chess_piece board[16][16]);
     void populate_board(int a, int b);	
 };	
  	
 /* Class Methods */
 //chess board	
-bool Chess_Board::update_board(char board[16][16])
+bool Chess_Board::update_board(chess_piece board[16][16])
 {
 	printf("   A B C D E F G H I J K L M N O P\n");//prints out the top of the board
 	for(int i =0; i< 16; i++)
@@ -45,8 +125,8 @@ bool Chess_Board::update_board(char board[16][16])
 		
 		for(int k =0; k < 16; k++)
 		{
-			printf(" %c", board[i][k]);
-		
+			printf(" %c", board[i][k].character);
+      //printf("%s",board[i][k].retrieve_name(board[i][k].character)); // so this is really messy, kind of an L tho not going to lie. prints out name of piece next to the piece
 		}
 		printf("\n");
 	}
@@ -75,7 +155,7 @@ void Chess_Board::populate_board(int a, int b)// fills up the board based on the
 			{
 				break;
 			}
-			board[iterator][i] = line[i]; // so this calls the golbal variable board, its messy but it works
+			board[iterator][i].character = line[i]; // so this calls the golbal variable board, its messy but it works
 		
 		}
 		iterator++;
