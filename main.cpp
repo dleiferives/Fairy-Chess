2c9ab6ae5b3f2ef4bd57fc301dd26441fe1bad37
Dylan Leifer-Ives <40322629+dleiferives@users.noreply.github.com>
2021-03-17 15:36:16 -0700

    added class for doing the stuff

--- main.cpp
+++ main.cpp
@@ -1,29 +1,35 @@
 /* Libraries */
 #include <cstdio> // pretty much everything, input and output only actually thinking about it now
-#include <stdlib.h> // i think this will let there be background coloring, i will have to check now, this could be poggers
+//think about including termcolor library to do color if thats a thing that is wanted. 
+
+/* Struct Definitions */
+struct chess_piece
+{
+  char character; // the thing that gets put onto the board
+  int ID; // id that represents the character
+  int x; // the position on the board the x
+  int y; // the position on the board the y
+  
+ char *retrieve_name(){};
+
+};
+
 /*Global Variables*/
 char board[16][16]; // our board
 
-/* Function Definitions */
-bool update_board(char board[16][16]);
-void populate_board(int a, int b);
-int main()
+/* Class Definitions */
+
+class Chess_Board
 {
-	system("Color E4");
-	populate_board(16,16);
-	while( true ) 
-	{
-		if( update_board(board) != true)
-		{
-		 break;
-		}
-	}
-	
-	return 0;
-}
-
-
-bool update_board(char board[16][16])
+  public:
+    /* Function Definitions */
+    bool update_board(char board[16][16]);
+    void populate_board(int a, int b);	
+};	
+ 	
+/* Class Methods */
+//chess board	
+bool Chess_Board::update_board(char board[16][16])
 {
 	printf("   A B C D E F G H I J K L M N O P\n");//prints out the top of the board
 	for(int i =0; i< 16; i++)
@@ -50,7 +56,7 @@ bool update_board(char board[16][16])
 	return false;
 }
 
-void populate_board(int a, int b)// fills up the board based on the specifications of play in board.txt
+void Chess_Board::populate_board(int a, int b)// fills up the board based on the specifications of play in board.txt
 {
 	FILE* fp = fopen("board.txt", "r"); // opens the file in reading mode
 	if (fp == NULL) // if there isnt L errors
@@ -77,3 +83,23 @@ void populate_board(int a, int b)// fills up the board based on the specificatio
 	fclose(fp);
 	
 }
+
+/* Methods */
+
+
+int main()
+{
+  Chess_Board new_board;
+  new_board.populate_board(16,16);
+  
+	while( true ) 
+	{
+		if( new_board.update_board(board) != true)
+		{
+		 break;
+		}
+	}
+	
+	return 0;
+}
+
