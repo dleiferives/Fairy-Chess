2f1ba002e9f3f44ca05fb3a0ffb9a71978b324ae
Dylan Leifer-Ives <40322629+dleiferives@users.noreply.github.com>
2021-03-17 16:43:18 -0700

    fixed spacing

--- main.cpp
+++ main.cpp
@@ -104,64 +104,64 @@ class Chess_Board
   public:
     /* Function Definitions */
     bool update_board(chess_piece board[16][16]);
-    void populate_board(int a, int b);	
-};	
- 	
+    void populate_board(int a, int b);  
+};  
+   
 /* Class Methods */
-//chess board	
+//chess board  
 bool Chess_Board::update_board(chess_piece board[16][16])
 {
-	printf("   A B C D E F G H I J K L M N O P\n");//prints out the top of the board
-	for(int i =0; i< 16; i++)
-	{
-	if( (i+1) < 10 ) // doing the numbers on the side
-		{
-			printf(" %i", (i+1) );
-		}
-		else
-		{
-			printf("%i", (i+1) );
-		}
-		
-		for(int k =0; k < 16; k++)
-		{
-			printf(" %c", board[i][k].character);
+  printf("   A B C D E F G H I J K L M N O P\n");//prints out the top of the board
+  for(int i =0; i< 16; i++)
+  {
+  if( (i+1) < 10 ) // doing the numbers on the side
+    {
+      printf(" %i", (i+1) );
+    }
+    else
+    {
+      printf("%i", (i+1) );
+    }
+    
+    for(int k =0; k < 16; k++)
+    {
+      printf(" %c", board[i][k].character);
       //printf("%s",board[i][k].retrieve_name(board[i][k].character)); // so this is really messy, kind of an L tho not going to lie. prints out name of piece next to the piece
-		}
-		printf("\n");
-	}
-	
-	
- 	
-	return false;
+    }
+    printf("\n");
+  }
+  
+  
+   
+  return false;
 }
 
 void Chess_Board::populate_board(int a, int b)// fills up the board based on the specifications of play in board.txt
 {
-	FILE* fp = fopen("board.txt", "r"); // opens the file in reading mode
-	if (fp == NULL) // if there isnt L errors
-	{
-		printf("shucks");
-	}
-	char *line = NULL; // amount of characters in line 16+ newline
-	size_t len = 16; // sixteen we dont want the newline
-	
-	int iterator =0;
-	while ((getline(&line, &len, fp)) != -1) 
-	{	
-		for( int i =0; i<b; i++)
-		{
-			if(iterator>= (a))
-			{
-				break;
-			}
-			board[iterator][i].character = line[i]; // so this calls the golbal variable board, its messy but it works
-		
-		}
-		iterator++;
-	}
-	fclose(fp);
-	
+  FILE* fp = fopen("board.txt", "r"); // opens the file in reading mode
+  if (fp == NULL) // if there isnt L errors
+  {
+    printf("shucks");
+  }
+  char *line = NULL; // amount of characters in line 16+ newline
+  size_t len = 16; // sixteen we dont want the newline
+  
+  int iterator =0;
+  while ((getline(&line, &len, fp)) != -1) 
+  {  
+    for( int i =0; i<b; i++)
+    {
+      if(iterator>= (a))
+      {
+        break;
+      }
+      board[iterator][i].character = line[i]; // so this calls the golbal variable board, its messy but it works
+    
+    }
+    iterator++;
+  }
+  fclose(fp);
+  
 }
 
 /* Methods */
@@ -172,14 +172,14 @@ int main()
   Chess_Board new_board;
   new_board.populate_board(16,16);
   
-	while( true ) 
-	{
-		if( new_board.update_board(board) != true)
-		{
-		 break;
-		}
-	}
-	
-	return 0;
+  while( true ) 
+  {
+    if( new_board.update_board(board) != true)
+    {
+     break;
+    }
+  }
+  
+  return 0;
 }
 
