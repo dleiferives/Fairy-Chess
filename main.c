0cc33560804086bf67d29e98757644721c125a69
dleiferives <dleiferives@gmail.com>
2021-04-20 21:23:34 -0700

    Diagonal checks complete

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MIN(a,b) ((a >b) ? b : a)
+
+
 //lets start with the board render
 
 
@@ -13,8 +16,14 @@ struct piece // defining piece
   int x;
   int y;
   char type;
+  // might replace with a bit field, not sure yet though
   int g_id;
   s_ptrp ptr; // self referencing pointer
+  // using a bit field to more effectively manage memory, it will do nothing special, but it is good practice 
+  unsigned int slide_diagonal : 1;
+  unsigned int slide_ortho : 1;
+  unsigned int jump : 1;
+  
 };
 
 struct board // defining board
@@ -63,14 +72,89 @@ int init_board(s_ptrb board_p) // from passed board input loads it with default 
  return 0;
 }
 
+int check_move(s_ptrb board, piece p, int p2x, int p2y)
+{
+ if(p.slide_diagonal == 1)
+ {
+   // generate all diagonal positions, 
+   //diagonal up left
+   int tl = MIN(p.x, p.y); // top left 
+   int tr = MIN(board->width - p.x, p.y); // top right
+   int br = MIN(board->width - p.x, (board->height) - p.y); // bottom right
+   int bl = MIN(p.x, board->height -p.y); // bottom left
+    
 
+   printf("x:%i  y:%i  bl:%i  tl:%i  tr:%i  br:%i\n width:%i  height:%i", p.x, p.y, bl, tl, tr, br, board->width, board->height);
+   if(0 < ((p2y-p.y)*(p2x - p.x)))
+   {
+    for(int i =0; i <tl; i++) // down left
+    {
+      //testing
+      board->tiles[((p.y - (i+1))*board->width) +p.x -(i+1)].type = '#';
+      //testing end
+      if( (p2x == (p.x - (i+1))) && ( p2y == (p.y -(i+1))) )
+      {
+      //  return 1;
+      }
+    }
+    for(int i =0; i < br; i++)
+    {
+      board->tiles[((p.y + (i+1))*board->width) +p.x +(i+1)].type = '#';
+
+      if( (p2x == (p.x + (i+1))) && (p2y == (p.y + (i+1))) )
+      {
+        //return 1;
+      }
+    }
+   //}
+  // else
+   //{
+
+    for(int i =0; i <bl; i++) // down left
+    {
+      //testing
+      board->tiles[((p.y + (i+1))*board->width) +p.x -(i+1)].type = '#';
+      //testing end
+      if( (p2x == (p.x - (i+1))) && ( p2y == (p.y +(i+1))) )
+      {
+      //  return 1;
+      }
+    }
+    for(int i =0; i < tr; i++)
+    {
+      board->tiles[((p.y - (i+1))*board->width) +p.x +(i+1)].type = '#';
+
+      if( (p2x == (p.x + (i+1))) && (p2y == (p.y - (i+1))) )
+      {
+        //return 1;
+      }
+    }
+   }
+
+
+
+
+ }
+ return 0;
+}
+
+int move_piece()
+{
+
+}
 
 // ## Main Function ##
 
 int main()
 {
-  board board = { 10, 10};
+  board board = {10, 10};
   init_board(&board);
+  board.tiles[11].type = 'n';
+  board.tiles[11].g_id = 1;
+  board.tiles[11].slide_diagonal = 1;
+  printf(" %i \n", check_move(&board, board.tiles[11], 4, 4));
+
   put_board(board.tiles, board.width, board.height);
+
   return 0;
 }
